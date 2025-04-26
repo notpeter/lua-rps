5093fbed62f571f70ced6302a4732a181fd73637
Peter Tripp <petertripp@gmail.com>
2025-04-26 09:15:49 -0400

    initial version (35 lines, 709 bytes source, 689 bytes bytecode)

---
+++ rps.lua
@@ -0,0 +1,35 @@
+table = { "rock", "paper", "scissors" }
+number = math.random(1, 3)
+print("Welcome to Rock Paper Scissors!\nChoose Your Move!")
+move = string.lower(io.read("*l"))
+if move == "rock" then
+    if number == 1 then
+        print("Tie!")
+    elseif number == 2 then
+        print("You Lose!")
+    else
+        print("You Win!")
+    end
+end
+
+if move == "paper" then
+    if number == 1 then
+        print("You Win!")
+    elseif number == 2 then
+        print("Tie!")
+    else
+        print("You Lose!")
+    end
+end
+
+if move == "scissors" then
+    if number == 1 then
+        print("You Lose!")
+    elseif number == 2 then
+        print("You Win!")
+    else
+        print("You Lose!")
+    end
+end
+
+print(table[number])
